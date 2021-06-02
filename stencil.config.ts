813f0a40ffad9f55786e4f3aca0da93175f49b08
John Gile <me@jgile.com>
2021-06-02 08:40:58 -0700

    wip

---
+++ stencil.config.ts
@@ -0,0 +1,32 @@
+import { Config } from '@stencil/core';
+import { postcss } from "@stencil/postcss";
+import autoprefixer from "autoprefixer";
+import tailwindcss from "tailwindcss";
+
+export const config: Config = {
+  namespace: 'components-for-john',
+  outputTargets: [
+    {
+      type: 'dist',
+      esmLoaderPath: '../loader',
+    },
+    {
+      type: 'dist-custom-elements-bundle',
+    },
+    {
+      type: 'docs-readme',
+    },
+    {
+      type: 'www',
+      serviceWorker: null, // disable service workers
+    },
+  ],
+  plugins: [
+    postcss({
+      plugins: [
+        tailwindcss("./tailwind.config.js"),
+        autoprefixer(),
+      ]
+    })
+  ]
+};
